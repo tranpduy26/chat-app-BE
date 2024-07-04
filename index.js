fa2d138536dff8fd0eebaafc0e4da3649e356b96
DuyTran <trandb26@gmail.com>
2024-07-04 11:17:31 +0700

    up DB

--- index.js
+++ index.js
@@ -10,42 +10,42 @@ app.use(cors());
 app.use(express.json());
 
 const MONGO_URL =
-	"mongodb+srv://trandb26:mbj7655oFhZQhTSY@cluster0.kpybdzc.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://admin:B9zO7wFqxJux7CyY@cluster0.kpybdzc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const port = 5000;
 
 mongoose
-	.connect(MONGO_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log("DB Connection Success!");
-	});
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Success!");
+  });
 
 app.use("/api/v1", rootRouter);
 
 const server = app.listen(port || process.env.PORT, () => {
-	console.log(`Server running on port ${port}!`);
+  console.log(`Server running on port ${port}!`);
 });
 
 const io = socket(server, {
-	cors: {
-		origin: "http://localhost:3000",
-		credentials: true,
-	},
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
 });
 
 global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-	global.chatSocket = socket;
-	socket.on("add-user", (userId) => {
-		onlineUsers.set(userId, socket.id);
-	});
-	socket.on("send-msg", (data) => {
-		const sendUserSocket = onlineUsers.get(data.to);
-		if (sendUserSocket) {
-			socket.to(sendUserSocket).emit("msg-receive", data.message);
-		}
-	});
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
 });
